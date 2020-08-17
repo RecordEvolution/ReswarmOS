ebb9bd33e9b8088d3f7ebc4f0bfadf04910ae493
Mario Fink <mario.fink@record-evolution.de>
2020-08-17 16:07:08 +0200

    switch to container build process

---
+++ LFS/logging.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 1. include function by $ source logging.sh
+# 2. call function by $ logging_timestamp_message "show some logging output"
+
+logging_timestamp_message()
+{
+	message="$1"
+	echo "$(tput setaf 2)[$(date +%Y-%m-%d_%H-%M-%S-%N)]$(tput sgr0) ${message}"
+}
