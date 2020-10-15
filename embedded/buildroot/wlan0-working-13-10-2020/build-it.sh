98925e267ea205cab8f5c300099c0376b9fdd9e7
Mario Fink <knif.oiram@gmail.com>
2020-10-15 13:18:15 +0200

    buildroot:build-it: fix buildroot path

--- embedded/buildroot/wlan0-working-13-10-2020/build-it.sh
+++ embedded/buildroot/wlan0-working-13-10-2020/build-it.sh
@@ -8,10 +8,10 @@ git clone https://github.com/buildroot/buildroot --single-branch --depth=1 ${wrk
 
 # copy buildroot configuration 
 cp buildroot_config-13-10-2020-18-26.conf ${wrkdir}/buildroot/.config
-cp wpa_supplicant.conf board/raspberrypi/
-cp post-build.sh board/raspberrypi/
+cp wpa_supplicant.conf ${wrkdir}/buildroot/board/raspberrypi/
+cp post-build.sh ${wrkdir}/buildroot/board/raspberrypi/
 
-pushd ${wrkdir}
+pushd ${wrkdir}/buildroot
 
 time make
 
