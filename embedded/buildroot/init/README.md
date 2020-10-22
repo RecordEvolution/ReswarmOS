6efcca59a67d9aad90590e2378d0946087cbbf15
Mario Fink <knif.oiram@gmail.com>
2020-10-22 18:55:02 +0200

    buildroot: resizefs debug

---
+++ embedded/buildroot/init/README.md
@@ -0,0 +1,2 @@
+
+- https://unix.stackexchange.com/questions/59018/create-and-control-start-up-scripts-in-busybox
--- embedded/buildroot/init/resize-os.sh
+++ embedded/buildroot/init/resize-os.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# - https://www.gnu.org/software/parted/manual/html_node/parted_31.html
 
 # disable devices for paging and swapping
 swapoff -a
@@ -9,6 +10,14 @@ disknam=$(lsblk -lo path,type | grep disk | awk '{print $1}' | tr -d ' ')
 # check partition table
 parted ${disknam} print
 fdisk -l | grep "${disknam}"
+
+# kill dockerd process (mounted the rootfs partition to /var/lib/docker)
+#pidcontainerd=$(ps aux | grep dockerd | grep -v grep | awk '{print $1}' | tr -d " ")
+#kill ${pidcontainerd}
+
+# unmount partition
+umount "${disknam}p2"
+lsblk
 
 # get number of sectors on disk
 numsectors=$(fdisk -l ${disknam} | grep "sectors$" | awk '{print $7}')
@@ -22,8 +31,13 @@ sectorend=$((numsectors-1))
 # delete second (root) partition
 parted --script ${disknam} rm 2
 
+# check partition table
+parted ${disknam} print
+fdisk -l | grep "${disknam}"
+
 # create new resized partition with exactly!! same start sector
-parted --script ${disknam} mkpart primary ext4 ${sectorstart} ${sectorend}
+echo "creating new partition from ${sectorstart} to ${sectorend}"
+parted --script ${disknam} mkpart primary ext4 ${sectorstart}s ${sectorend}s
 
 # instead of rebooting let the kernel detect the new partition table
 partprobe
@@ -35,6 +49,9 @@ df -h
 
 # adjust/resize filesystem to new partition
 resize2fs -p "${disknam}p2"
+
+# remount filesystem
+mount -all
 
 # check partition/filesystem
 df -h
