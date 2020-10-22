6499c126c670f72eeaad56360680c487d99f35e3
Mario Fink <knif.oiram@gmail.com>
2020-10-22 18:14:10 +0200

    buildroot: debug resize.sh

--- embedded/buildroot/init/resize-os.sh
+++ embedded/buildroot/init/resize-os.sh
@@ -3,14 +3,27 @@
 # disable devices for paging and swapping
 swapoff -a
 
-# print partition table
-fdisk -l
+# get name of physical disk
+disknam=$(lsblk -lo path,type | grep disk | awk '{print $1}' | tr -d ' ')
 
-# delete root partition and recreate it
-fdisk /dev/mmcblk0
-# 1. d  -> 2 
-# 2. n -> 2 -> 65537 - (N-1)Sector
-# switch to $ part  instead of fdisk to automate partition manipulation
+# check partition table
+parted ${disknam} print
+fdisk -l | grep "${disknam}"
+
+# get number of sectors on disk
+numsectors=$(fdisk -l ${disknam} | grep "sectors$" | awk '{print $7}')
+
+# get index of first sector of second partition
+sectorstart=$(fdisk -l | grep "${disknam}p2" | awk '{print $2}')
+
+# calculate last sector
+sectorend=$((numsectors-1))
+
+# delete second (root) partition
+parted --script ${disknam} rm 2
+
+# create new resized partition with exactly!! same start sector
+parted --script ${disknam} mkpart primary ext4 ${sectorstart} ${sectorend}
 
 # instead of rebooting let the kernel detect the new partition table
 partprobe
@@ -20,10 +33,13 @@ partprobe
 lsblk
 df -h
 
-# resize filesystem
-resize2fs /dev/mmcblk0p2
+# adjust/resize filesystem to new partition
+resize2fs -p "${disknam}p2"
 
+# check partition/filesystem
 df -h
+parted ${disknam} print
+fdisk -l | grep "${disknam}"
 
 # reboot device
 #reboot
