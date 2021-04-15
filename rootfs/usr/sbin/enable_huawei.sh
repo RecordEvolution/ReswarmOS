c914507463a8deceaa289aa68ab5be83954dc003
Mario Fink <knif.oiram@gmail.com>
2021-04-15 18:08:45 +0200

    introduce usb_modeswitch.conf to enable huawei LTE stick

---
+++ rootfs/usr/sbin/enable_huawei.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "triggering udev rule 40-huawei-lte.rules at $(date)" > /var/log/udev-huawei.log
+
+usb_modeswitch -J -c /etc/usb_modeswitch.conf
+
+echo "finished udev rule 40-huawei-lte.rules at $(date)" > /var/log/udev-huawei.log
+
+# check device properties:
+# $ udevadm info -a -n /dev/sr0
+#
+# monitor udev:
+# $ udevadm monitor
+#
+# enter properties in udev rule:
+# ATTRS{idVendor}=="12d1", ATTRS{manufacturer}!="Android", ATTR{bInterfaceNumber}=="00", ATTR{bInterfaceClass}=="08", RUN+="usb_modeswitch '%b/%k'"
