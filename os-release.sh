868d86b37f04c39ff4b32a1ef1f8b93a69eb03b7
Mario Fink <knif.oiram@gmail.com>
2021-04-20 11:58:30 +0200

    use current 'buildtime' timestamp for os-release

--- os-release.sh
+++ os-release.sh
@@ -3,7 +3,8 @@
 vrsn=$(cat config.yaml | grep version | awk -F ':' '{print $2}' | tr -d ' ')
 gthsh=$(git rev-parse HEAD)
 gthshshort=$(git rev-parse --short HEAD)
-tsdate=$(git log -1 --format=%cd --date=format:"%Y%m%dT%H%M%S")
+# tsdate=$(git log -1 --format=%cd --date=format:"%Y%m%dT%H%M%S")
+tsdate=$(date +%Y%m%dT%H%M%S)
 
 osrls=$(cat << EOF
 NAME=ReswarmOS
