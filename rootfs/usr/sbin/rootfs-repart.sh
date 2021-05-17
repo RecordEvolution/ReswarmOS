15134d26a9ac4e1a12f4306f81f50c851581e27d
Mario Fink <mario.fink@record-evolution.de>
2021-05-17 19:05:03 +0200

    improve/fix stability of initial boot rootfs resizing => issue #41, version 0.3.7

--- rootfs/usr/sbin/rootfs-repart.sh
+++ rootfs/usr/sbin/rootfs-repart.sh
@@ -1,25 +1,50 @@
 #!/bin/sh
 
-# specify device name and its partition to check
-devnm="mmcblk0"
-devprt="2"
+rootpart="$(findmnt -n -o SOURCE /)"
+rootdev="/dev/$(lsblk -no pkname "$rootpart")"
 
-# check size of partitions and compare to disk capacity
-dskflsz=$(cat /sys/class/block/${devnm}/size)
-dskptsz=$(cat /sys/class/block/${devnm}p${devprt}/size)
-echo "${devnm}:   ${dskflsz}"
-echo "${devnm}p${devprt}: ${dskptsz}"
-dskptsz2=$((2*dskptsz))
-#echo ${dskptsz2}
+flock $rootdev sfdisk -f $rootdev -N 2 <<EOF
+,+
+EOF
 
-if [ ${dskptsz2} -lt ${dskflsz} ]; then
-	echo "partition should be resized"
-	# resize partition
-	parted --script /dev/${devnm} resizepart ${devprt} 100%
-	# resize filesystem
-	resize2fs /dev/${devnm}p${devprt}
-	# inform kernel about new partition table
-	partprobe
-else
-	echo "partition seems to occupy most of the disk"
-fi
+sleep 5
+
+udevadm settle
+
+sleep 5
+
+flock $rootdev partprobe $rootdev
+
+mount -o remount,rw $rootpart
+
+resize2fs $rootpart
+
+exit 0
+
+# for reference, see
+# https://salsa.debian.org/raspi-team/image-specs/-/blob/master/rootfs/usr/sbin/rpi-resizerootfs
+
+## specify device name and its partition to check
+#devnm="mmcblk0"
+#devprt="2"
+#
+## check size of partitions and compare to disk capacity
+#dskflsz=$(cat /sys/class/block/${devnm}/size)
+#dskptsz=$(cat /sys/class/block/${devnm}p${devprt}/size)
+#echo "${devnm}:   ${dskflsz}"
+#echo "${devnm}p${devprt}: ${dskptsz}"
+#dskptsz2=$((2*dskptsz))
+##echo ${dskptsz2}
+#
+#if [ ${dskptsz2} -lt ${dskflsz} ]; then
+#	echo "partition should be resized"
+#	# resize partition
+#	parted --script /dev/${devnm} resizepart ${devprt} 100%
+#	# inform kernel about new partition table
+#	partprobe
+#	# resize filesystem
+#        #e2fsck -f /dev/${devnm}p${devprt}
+#	resize2fs /dev/${devnm}p${devprt}
+#else
+#	echo "partition seems to occupy most of the disk"
+#fi
