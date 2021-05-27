c7c4491ff136ed1dd5ba2ee4db623bc202471677
Mario Fink <mario.fink@record-evolution.de>
2021-05-27 22:49:30 +0000

    * reswarmify/network-config.sh: disable netplan.io to let NetworkManager   manage eth0 as well * rename reswarmify script according to their order

--- reswarmify/08-network-config.sh
+++ reswarmify/08-network-config.sh
@@ -21,13 +21,18 @@ logging_message "rootfs mount point: ${rootfsmntpnt}"
 # either remove:
 #apt remove netplan.io
 # or disable it
+echo "disable netplan.io"
 if [ ! -d ${rootfsmntpnt}/etc/cloud/cloud.cfg.d/ ]; then
   mkdir -pv ${rootfsmntpnt}/etc/cloud/cloud.cfg.d
 fi
 echo "network: {config: disabled}" > ${rootfsmntpnt}/etc/cloud/cloud.cfg.d/97-disable-network-config.cfg
+ls -lh ${rootfsmntpnt}/etc/cloud/cloud.cfg.d/
+cat ${rootfsmntpnt}/etc/cloud/cloud.cfg.d/97-disable-network-config.cfg
 #systemctl disable systemd-networkd.service
 #systemctl mask systemd-networkd.service
 #systemctl status systemd-networkd.service | cat
+# that basically did the trick!! =>...
+mv -v /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.unused
 
 sleep 2
 
