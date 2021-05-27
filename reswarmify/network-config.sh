d047efe57e860cf67fed1d79c7b227ee45d892e6
Mario Fink <mario.fink@record-evolution.de>
2021-05-27 18:41:00 +0200

    reswarmify: split network, partition config

---
+++ reswarmify/network-config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source logging.sh
+
+# 1st argument: root filesystem mount point
+rootfsmntpnt="$1" # = "/" when installing rootfs overlay on a running system
+
+if [ -z "${rootfsmntpnt}" ]; then
+  logging_error "rootfs_install.sh: missing argument 'rootfsmntpnt'"
+  exit 1
+fi
+
+#-----------------------------------------------------------------------------#
+
+logging_header "configure networking"
+
+logging_message "rootfs mount point: ${rootfsmntpnt}"
+
+# make all network interfaces (including eth0) managed by NetworkManager
+# Ubuntu > 17.10
+# either remove:
+#apt remove netplan.io
+# or disable it
+if [ ! -d ${rootfsmntpnt}/etc/cloud/cloud.cfg.d/ ]; then
+  mkdir -pv ${rootfsmntpnt}/etc/cloud/cloud.cfg.d
+fi
+echo "network: {config: disabled}" > ${rootfsmntpnt}/etc/cloud/cloud.cfg.d/97-disable-network-config.cfg
+#systemctl disable systemd-networkd.service
+#systemctl mask systemd-networkd.service
+#systemctl status systemd-networkd.service | cat
+
+sleep 2
+
+#-----------------------------------------------------------------------------#
