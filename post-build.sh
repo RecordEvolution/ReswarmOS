882584f9c388230cdc998dfaf03e7f48c4cb0374
Mario Fink <mario.fink@record-evolution.de>
2021-06-17 17:05:50 +0200

    include u-boot + rauc

--- post-build.sh
+++ post-build.sh
@@ -21,3 +21,9 @@ ln -s /dev/null ${TARGET_DIR}/etc/systemd/system/serial-getty@ttyS0.service
 # disable/mask (preliminary) reagent upgrade services
 rm -vf ${TARGET_DIR}/etc/systemd/system/reagent-upgrade.service
 ln -s /dev/null ${TARGET_DIR}/etc/systemd/system/reagent-upgrade.service
+
+# set up soft link (in agent directory) pointing to mount point of vfat partition
+vfatlnk="${TARGET_DIR}/opt/reagent/vfat-mount"
+bootmnt=$(cat ${TARGET_DIR}/etc/fstab | grep -i vfat | awk '{print $2}' | tr -d ' ')
+#ln -s ${bootmnt} ${vfatlnk}
+
