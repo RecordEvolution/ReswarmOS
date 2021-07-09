53346ee75279b67ed29dbe98fcdbf5b392fd045a
Mario Fink <mario.fink@record-evolution.de>
2021-07-09 16:50:07 +0200

    * board/model specific U-Boot script -> uboot.sh * RAUC service fully functional

--- config/raspberrypi/raspberrypi3/post-build.sh
+++ config/raspberrypi/raspberrypi3/post-build.sh
@@ -10,17 +10,20 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
+# define container (volume) home directory
+BUILD_DIR=/home/buildroot/
+
 # convert boot-script and kernel into U-boot image format
 # for reference also see: https://rauc.readthedocs.io/en/latest/integration.html#set-up-u-boot-boot-script-for-rauc
-mkimage -A arm -O linux -T script -C none -n "U-boot script" -d /home/buildroot/boot/uboot/boot.txt ${BINARIES_DIR}/boot.scr.uimg
+mkimage -A arm -O linux -T script -C none -n "U-boot script" -d ${BUILD_DIR}/config/raspberrypi/raspberrypi3/uboot.sh ${BINARIES_DIR}/boot.scr.uimg
 mkimage -A arm -O linux -T kernel -C none -a 0x00008000 -e 0x00008000 -n "Linux kernel" -d ${BINARIES_DIR}/zImage ${BINARIES_DIR}/uImage
 
 # include all customized boot directory files
-ls /home/buildroot/boot/ -lhR
-cp -v /home/buildroot/boot/cmdline.txt ${BINARIES_DIR}/
-cp -v /home/buildroot/boot/config.txt ${BINARIES_DIR}/
-cp -v /home/buildroot/boot/device.ini ${BINARIES_DIR}/
-cp -v /home/buildroot/boot/uboot/uboot.env ${BINARIES_DIR}/
+ls ${BUILD_DIR}/boot/ -lhR
+#cp -v ${BUILD_DIR}/boot/cmdline.txt ${BINARIES_DIR}/
+cp -v ${BUILD_DIR}/boot/config.txt ${BINARIES_DIR}/
+cp -v ${BUILD_DIR}/boot/device.ini ${BINARIES_DIR}/
+cp -v ${BUILD_DIR}/boot/uboot/uboot.env ${BINARIES_DIR}/
 
 # disable/mask ttyS0
 rm -vf ${TARGET_DIR}/etc/systemd/system/serial-getty@ttyS0.service
---
+++ config/raspberrypi/raspberrypi3/uboot.sh
@@ -0,0 +1,22 @@
+
+# show board and flash info 
+bdinfo
+flinfo
+
+# load device-tree and kernel
+fatload mmc 0:1 ${fdt_addr_r} bcm2710-rpi-3-b.dtb
+fatload mmc 0:1 ${kernel_addr_r} uImage
+
+# set boot arguments corresponding to cmdline.txt
+setenv bootargs root=/dev/mmcblk0p2 rootfstype=ext4 rootwait rw console=tty1 console=ttyAMA0,115200 cgroup_enable=cpuset cgroup_enable=memory swapaccount=1 fsck.repair=yes dwc_otg.lpm_enable=0
+
+# RAUC 
+setenv BOOT_ORDER "rootfs0 rootfs1"
+setenv BOOT_rootfs0_LEFT 3
+setenv BOOT_rootfs1_LEFT 3
+
+# save environment variables
+saveenv
+
+# boot kernel (and device-tree)
+bootm ${kernel_addr_r} - ${fdt_addr_r}
