c277dd065686c43ed2d3d30f841b2c0bda300fd2
Mario Fink <mario.fink@record-evolution.de>
2021-08-27 17:22:58 +0200

    * build-agent.sh: preliminary solution for setting env GOARM=6,7 for pizero vs. pi2,3,4 * raspiberrypi0w config: adjust rootfs partition label * raspberrypi3 config: add missing PROCPS_NG package * raspberrypi3 genimage.cfg: disable (preliminary) multi-partition layout

--- build-agent.sh
+++ build-agent.sh
@@ -20,15 +20,27 @@ fi
 # golang (cross-) compiler path
 GOC=${BASE_DIR}/host/bin/go
 
-# get required architecture from configuration file
+# get required architecture/version from configuration file
 ARCH=$(cat ${BR2_CONFIG} | grep 'BR2_ARCH=' | awk -F '=' '{print $2}' | tr -d '" ')
-echo "building for architecture: ${ARCH}"
+ARCV=$(cat ${BR2_CONFIG} | grep 'BR2_arm1176j' | grep -v "^#" | awk -F '=' '{print $1}')
+echo "building for architecture: ${ARCH} (${ARCV})"
 
 # build agent binary
-if [ ! -f ${BASE_DIR}/build/DeviceManagementAgent/src/reagent ]; then
+#if [ ! -f ${BASE_DIR}/build/DeviceManagementAgent/src/reagent ]; then
+if [ 0 == 0 ]; then
   pushd ${BASE_DIR}/build/DeviceManagementAgent/src/
   ${GOC} get .
-  GOOS=linux GOARCH=${ARCH} CGO_ENABLED=1 ${GOC} build .
+  # for reference, see:
+  # - https://github.com/goreleaser/goreleaser/issues/36
+  #GOOS=linux GOARCH=${ARCH} CGO_ENABLED=1 GOARM=6(Pi A,B,...,Zero),7(Pi 2,3,4) ${GOC} build .
+  # TODO use proper a build-system in DeviceManagementAgent
+  if [ -z "${ARCV}" ]; then
+    echo "building for GOARM=7"
+    CGO_ENABLED=1 GOOS=linux GOARCH=${ARCH} GOARM=7 ${GOC} build .
+  else
+    echo "building for GOARM=6"
+    CGO_ENABLED=1 GOOS=linux GOARCH=${ARCH} GOARM=6 ${GOC} build .
+  fi
   popd
 fi
 
