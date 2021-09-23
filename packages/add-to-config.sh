94b13187cb6f6c5f3270074a5e0f70c8cafeba83
Mario Fink <mario.fink@record-evolution.de>
2021-09-23 17:04:17 +0200

    * update raspberry3 config to up-to-date buildroot version * packages/add-to-config.sh script finished * add preliminary reagent package as external buildroot package * shadow.mk: fix buildsystem with autotools package flag

--- packages/add-to-config.sh
+++ packages/add-to-config.sh
@@ -2,12 +2,67 @@
 
 relPathConfig="$1"
 if [ -z "${relPathConfig}" ]; then
-  echo "missing relative path to buildroot/packages/Config.in" >&2
+  echo "missing path to buildroot/packages/Config.in" >&2
   exit 1
 fi
 
+addEntry()
+{
+  # full configuration
+  fullConfig="$1"
+  # name of menu the new entry is supposed to be inserted in
+  menuConfig="$2"
+  # new entry, i.e. path of package`s Config.in
+  menuEntry="$3"
+
+  if [ -z "${fullConfig}" ]; then
+    echo "fullConfig argument is missing" >&2
+    return 1
+  fi
+  if [ -z "${menuConfig}" ]; then
+    echo "menuConfig argument is missing" >&2
+    return 1
+  fi
+  if [ -z "${menuEntry}" ]; then
+    echo "menuEntry argument is missing" >&2
+    return 1
+  fi
+
+  # extract required menu section
+  sectionList=$(echo -e "${fullConfig}" | grep "${menuConfig}" -A 99999 | grep "endmenu" -m1 -B 99999)
+  # remove prefix/postfix 'menu'/'endmenu'
+  cleanList=$(echo -e "${sectionList}" | grep -v '^menu' | grep -v '^endmenu')
+  # append new entry and sort all entries
+  newList=$(echo -e "${cleanList}\n\t${menuEntry}")
+  newListSorted=$(echo -e "${newList}" | sort)
+
+  # replace newlines with some other entity in both original and new list
+  clnList=$(echo -e "${cleanList}" | tr '\n' '\Z' | sed 's/\//\\\//g')
+  sedList=$(echo -e "${newListSorted}" | tr '\n' '\Z' | sed 's/\//\\\//g')
+
+  # replace newlines in full Config.in
+  fullList=$(echo -e "${fullConfig}" | tr '\n' '\Z')
+
+  # perform replacement of old with new package list in specific menu section
+  fullListRepl=$(echo "${fullList}" | sed "s/${clnList}/${sedList}/g" | tr '\Z' '\n')
+
+  echo -e "${fullListRepl}"
+}
+
+# --------------------------------------------------------------------------- #
+
+# get full config
+fullConfig=$(cat ${relPathConfig})
+
+# list packages with the menu name they are supposed to go in and their entry
+
+# ...shadow
 shadowMenu="Miscellaneous"
 shadowEntry="source \"package/shadow/Config.in\""
+fullConfig=$(addEntry "${fullConfig}" "${shadowMenu}" "${shadowEntry}")
 
-cat "${relPathConfig}" | grep "${shadowMenu}" -A 99999 | grep "endmenu" -m1 -B 99999
+# ...next package...
+# ...
+
+echo -e "${fullConfig}"
 
