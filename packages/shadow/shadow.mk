00bed395995ab2e13381e317d4063705b93697b4
Mario Fink <mario.fink@record-evolution.de>
2021-09-23 17:04:17 +0200

    * update raspberry3 config to up-to-date buildroot version * packages/add-to-config.sh script finished * add preliminary reagent package as external buildroot package * shadow.mk: fix buildsystem with autotools package flag

--- packages/shadow/shadow.mk
+++ packages/shadow/shadow.mk
@@ -5,20 +5,11 @@
 ################################################################################
 
 SHADOW_VERSION = 4.9
-SHADOW_SOURCE = shadow$(subst .,,$(SHADOW_VERSION)).tgz
-SHADOW_SITE = https://github.com/shadow-maint/shadow
+SHADOW_SOURCE = shadow-$(SHADOW_VERSION).tar.gz
+SHADOW_SITE = https://github.com/shadow-maint/shadow/releases/download/v$(SHADOW_VERSION)
 SHADOW_LICENSE = 
 SHADOW_LICENSE_FILES = 
+SHADOW_CONF_OPTS = 
 SHADOW_DEPENDENCIES = 
 
-define SHADOW_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
-		XLIBPATH=$(STAGING_DIR)/usr/lib
-endef
-
-define SHADOW_INSTALL_TARGET_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
-		XLIBPATH=$(STAGING_DIR)/usr/lib PREFIX=$(TARGET_DIR)/usr install
-endef
-
-$(eval $(generic-package))
+$(eval $(autotools-package))
