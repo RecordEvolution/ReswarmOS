d6d10f6386c20bf6a1debd6855c7f69f8086d23d
Mario Fink <knif.oiram@gmail.com>
2021-10-20 18:45:15 +0200

    debug and test rewarmify-nvidia.sh

--- config/nvidia/reswarmify-nvidia.sh
+++ config/nvidia/reswarmify-nvidia.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# check .reswarm file argument
+reswarmfile="$1"
+if [ -z "${reswarmfile}" ]; then
+  echo "please provide .reswarm file path" >&2
+  exit 1
+fi
+
 # update and install docker and vim
 apt-get update && apt-get install -y docker.io vim
 
@@ -17,9 +24,61 @@ apt-get update && apt-get install -y jq
 sleep 2
 
 # download Reagent for aarch64 architecture
-wget https://storage.googleapis.com/re-agent/linux/arm64/0.6.3.1/reagent -P ~/Downloads/
+wget https://storage.googleapis.com/re-agent/linux/arm64/0.6.3.1/reagent -P ./
 
 # create Reagent directory
 mkdir -pv /opt/reagent/
-mv -v ~/Downloads/reagent /opt/reagent/reagent-latest
+mv -v ./reagent /opt/reagent/reagent-latest
+chmod +x /opt/reagent/reagent-latest
+
+# Reagent systemd service
+reagentservice=$(cat << EOF
+[Unit]
+Description=Reagent
+ConditionPathExists=/opt/reagent/reswarm-mode
+After=network-online.target
+Requires=network-online.target
+
+[Service]
+Type=simple
+ExecStart=/opt/reagent/reagent-latest -config /opt/reagent/device-config.reswarm -appsDir /apps -debug
+RestartSec=30
+Restart=yes
+Restart=always
+CPUWeight=2048
+Nice=-16
+# CPUAccounting=1
+# MemoryAccounting=1
+# BlockIOAccounting=1
+# IPAccounting=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+)
+
+# activate Reswarm mode
+touch /opt/reagent/reswarm-mode
+
+# set up the service
+echo -e "${reagentservice}" > reagent.service
+mv -v reagent.service /etc/systemd/system/reagent.service
+systemctl enable reagent.service
+
+# configure .reswarm file
+cp -v "${reswarmfile}" /opt/reagent/device-config.reswarm
+chown root:root /opt/reagent/device-config.reswarm
+
+ls -lh /opt/reagent/
+
+# create apps directory
+mkdir -pv /apps
+ls -lhd /apps
+
+# start the Reagent service
+systemctl restart reagent.service
+
+sleep 5
+
+systemctl status reagent.service | cat
 
