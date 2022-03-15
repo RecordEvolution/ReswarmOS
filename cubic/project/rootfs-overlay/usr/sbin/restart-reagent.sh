85119b5372945135d0e2731c69404252150d7769
DewitteRuben <rubendewitte1998@gmail.com>
2022-03-15 23:27:14 +0100

    fix: properly restart on network change (nm)

---
+++ cubic/project/rootfs-overlay/usr/sbin/restart-reagent.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LGFL="/var/log/nm-dispatcher.log"
+schedule_lock_file="/opt/reagent/schedule-lock"
+
+if [ -f "$schedule_lock_file" ]; then
+    echo "A restart has already been scheduled" >>"${LGFL}"
+    exit 0
+fi
+
+touch $schedule_lock_file
+echo "Restarting reagent in 3 seconds..." >>"${LGFL}"
+
+sleep 3
+systemctl restart reagent
+
+echo "Restarted the agent service!\n" >>"${LGFL}"
+
+rm $schedule_lock_file
