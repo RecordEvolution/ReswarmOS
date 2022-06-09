0d2da2f232971673370fefb8dc79651f43188841
DewitteRuben <rubendewitte1998@gmail.com>
2022-06-09 16:51:13 +0200

    chore: move files

--- demo/network-manager.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-nmcli radio
-
-nmcli device
-
-nmcli device wifi rescan
-nmcli device wifi list
-nmcli device wifi connect SSID-Name password wireless-password
--- demo/traffic-limit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-tc qdisc add dev eth0 root tbf rate 1200kbit latency 50ms burst 1400
